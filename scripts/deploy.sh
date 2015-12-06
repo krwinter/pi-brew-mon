e8f7e33dac7c148d153b7992f75e02054db02fb1
krwinter <krwinter@gmail.com>
2015-12-06 02:35:06 -0700

    have deploy scripts and web folder

---
+++ scripts/deploy.sh
@@ -0,0 +1,4 @@
+#deploy to pi
+
+scp -r -i /Users/kwinter/.ssh/id_rsa_pi /Users/kwinter/Documents/workspaces/pi/pi-brew-mon/web ken@pi:/home/ken/pi-brew-mon
+scp -i /Users/kwinter/.ssh/id_rsa_pi -r /Users/kwinter/Documents/workspaces/pi/pi-brew-mon/scripts ken@pi:/home/ken/pi-brew-mon
