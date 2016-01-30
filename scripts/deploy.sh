f6602855dc0ca53f9cd6b7210f257297fab1d4d8
krwinter <krwinter@gmail.com>
2016-01-29 22:25:32 -0700

    interim checkin

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,8 @@
 #deploy to pi
+rsync -avx --delete-excluded -e 'ssh -i /Users/kwinter/.ssh/id_rsa_pi' --progress /Users/kwinter/Documents/workspaces/pi/node-restify-api-gateway/app ken@pi:/home/ken/brewPi/node-pi
+rsync -avx --delete-excluded -e 'ssh -i /Users/kwinter/.ssh/id_rsa_pi' /Users/kwinter/Documents/workspaces/pi/pi-brew-mon/scripts ken@pi:/home/ken/brewPi/pi-brew-mon
 
-scp -r -i /Users/kwinter/.ssh/id_rsa_pi /Users/kwinter/Documents/workspaces/pi/pi-brew-mon/web ken@pi:/home/ken/brewPi/pi-brew-mon
-scp -i /Users/kwinter/.ssh/id_rsa_pi -r /Users/kwinter/Documents/workspaces/pi/pi-brew-mon/scripts ken@pi:/home/ken/brewPi/pi-brew-mon
-scp -i /Users/kwinter/.ssh/id_rsa_pi -r /Users/kwinter/Documents/workspaces/pi/node-restify-api-gateway/app ken@pi:/home/ken/brewPi/node-pi
+
+# -r -i /Users/kwinter/.ssh/id_rsa_pi /Users/kwinter/Documents/workspaces/pi/pi-brew-mon/web ken@pi:/home/ken/brewPi/pi-brew-mon
+#scp -i /Users/kwinter/.ssh/id_rsa_pi -r /Users/kwinter/Documents/workspaces/pi/pi-brew-mon/scripts ken@pi:/home/ken/brewPi/pi-brew-mon
+#scp -i /Users/kwinter/.ssh/id_rsa_pi -r /Users/kwinter/Documents/workspaces/pi/node-restify-api-gateway/app ken@pi:/home/ken/brewPi/node-pi
